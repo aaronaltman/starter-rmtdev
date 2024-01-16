55b962e7662988491f1a6804992c4ba2c69d9c5d
aka <aaronkaltman@gmail.com>
2024-01-16 17:56:16 -0500

    ok still not working but understanding

--- src/components/JobListItem.tsx
+++ src/components/JobListItem.tsx
@@ -1,13 +1,27 @@
 import BookmarkIcon from "./BookmarkIcon";
 
-export default function JobListItem() {
+type JobItem = {
+  id: number;
+  badgeLetters: string;
+  title: string;
+  company: string;
+  date: string;
+  relevanceScore: number;
+  daysAgo: number;
+};
+
+type JobListItemProps = {
+  jobItem: JobItem;
+};
+
+export default function JobListItem({ jobItem }: JobListItemProps) {
   return (
     <li className="job-item">
       <a className="job-item__link">
         <div className="job-item__badge">9T</div>
 
         <div className="job-item__middle">
-          <h3 className="third-heading">Front End React Engineer</h3>
+          <h3 className="third-heading">{jobItem.title}</h3>
           <p className="job-item__company">9th Tech</p>
         </div>
 
